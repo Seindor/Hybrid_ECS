785ca5a56b496c0d56e0b1e89dfa6880d9ce3b3e
Seindor <xxseindorxx28@gmail.com>
2026-09-21 22:26:46 +0300

    Matches

--- scripts/bash/serve.sh
+++ scripts/bash/serve.sh
@@ -9,7 +9,13 @@ fi
 rm -rf out
 rojo sourcemap default.project.json -o sourcemap.json
 
-ROBLOX_DEV=true darklua process --config .darklua.json --watch src/ out/ \
-    & rojo serve build.project.json \
-    & rojo sourcemap default.project.json -o sourcemap.json --watch 
-    
+# первый проход синхронно: out/ точно существует, ошибки видны сразу
+ROBLOX_DEV=true darklua process --config .darklua.json src/ out/
+
+trap 'kill $(jobs -p) 2>/dev/null' EXIT INT TERM
+
+ROBLOX_DEV=true darklua process --config .darklua.json --watch src/ out/ &
+rojo serve build.project.json &
+rojo sourcemap default.project.json -o sourcemap.json --watch &
+
+wait
